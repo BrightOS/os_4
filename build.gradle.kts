4685354a0ded15074c53677e603ecd1bf3fcef3a
BrightOS <dbarov3@gmail.com>
2023-05-19 00:11:18 +0500

    fix some problems in gradle files

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,9 @@ plugins {
     id("com.android.application") version "8.1.0-beta02" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
 }
+
+    ext {
+        val appCompatVersion: String by extra("1.6.1")
+        val materialVersion: String by extra("1.8.0")
+        val coreKtxVersion: String by extra("1.9.0")
+    }
